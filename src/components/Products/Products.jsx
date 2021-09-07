ac2fe3c16c46d981311b8b7aa6672dd2886db6fc
glenvasa <70590043+glenvasa@users.noreply.github.com>
2021-09-07 10:45:57 -0400

    Create Products.jsx

---
+++ src/components/Products/Products.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { Grid } from "@material-ui/core";
+import Product from "./Product/Product";
+
+const products = [
+  { id: 1, name: "Shoes", description: "Puma Running Shoes.", price: "$50", image: 'https://images.puma.net/images/189074/08/sv01/fnd/PNA/' },
+  { id: 2, name: "Macbook", description: "Apple Macbook.", price: "$900", image: 'https://s.yimg.com/os/creatr-uploaded-images/2020-12/d37e0460-3eeb-11eb-b7f9-449e864b9135' },
+];
+
+const Products = () => {
+  return (
+    <main>
+      <Grid container justify="center" spacing={4}>
+        {products.map((product) => (
+          <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
+            <Product product={product} />
+          </Grid>
+        ))}
+      </Grid>
+    </main>
+  );
+};
+
+export default Products;
