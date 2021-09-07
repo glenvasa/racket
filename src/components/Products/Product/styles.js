3f67aab1eceaf48891972c57ea241488636d369a
glenvasa <70590043+glenvasa@users.noreply.github.com>
2021-09-07 10:45:53 -0400

    Create styles.js

---
+++ src/components/Products/Product/styles.js
@@ -0,0 +1,20 @@
+import { makeStyles } from "@material-ui/core/styles";
+
+// cb is only parameter w/instant return; object wrapped in ()
+export default makeStyles(() => ({
+  root: {
+    maxWidth: "100%",
+  },
+  media: {
+    height: 0,
+    paddingTop: "56.25%", // 16:9
+  },
+  cardActions: {
+    display: "flex",
+    justifyContent: "flex-end",
+  },
+  cardContent: {
+    display: "flex",
+    justifyContent: "space-between",
+  },
+}));
