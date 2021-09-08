262a803eb7d61a5c2d5152429af25e74ec2fd70b
glenvasa <70590043+glenvasa@users.noreply.github.com>
2021-09-08 09:52:51 -0400

    created fetchCart and handleAddToCart fns
    passing handleAddToCart as prop to Products and then down to Product

--- src/App.js
+++ src/App.js
@@ -2,23 +2,39 @@ import React, {useState, useEffect} from 'react'
 import { commerce} from './lib/commerce'
 import { Products, Navbar} from './components'
 
+
 const App = () => {
     const [products, setProducts] = useState([])
+    const [cart, setCart] = useState({})
+    
 
     const fetchProducts = async () => {
         const { data } = await commerce.products.list()
 
         setProducts(data)
     }
+    const fetchCart = async () => {
+        setCart(await commerce.cart.retrieve())
+    }
+
+    const handleAddToCart = async (productId, quantity) => {
+        const item = await commerce.cart.add(productId, quantity)
+
+        // set Cart state to updated cart after item has been added
+        setCart(item.cart)
+    }
 
     useEffect(() => {
         fetchProducts()
+        fetchCart()
     }, [])
+
+    console.log(cart)
 
     return (
        <div>
            <Navbar/>
-           <Products products={products}/>
+           <Products products={products} onAddToCart={handleAddToCart} />
        </div>
     )
 }
