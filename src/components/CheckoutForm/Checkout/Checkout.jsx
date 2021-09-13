173f3fbb94a36c3d27106ae2b36ec81b2590d6a6
glenvasa <70590043+glenvasa@users.noreply.github.com>
2021-09-13 10:50:31 -0400

    created generateToken fn to obtain checkout token as soon as user enters Checkout screen

--- src/components/CheckoutForm/Checkout/Checkout.jsx
+++ src/components/CheckoutForm/Checkout/Checkout.jsx
@@ -1,25 +1,32 @@
-import React, { useState } from "react";
-import {
-  Paper,
-  Stepper,
-  Step,
-  StepLabel,
-  Typography,
-  CircularProgress,
-  Divider,
-  Button,
-} from "@material-ui/core";
-
+import React, { useState, useEffect } from "react";
+import { Paper, Stepper, Step, StepLabel, Typography, CircularProgress, Divider, Button } from "@material-ui/core";
+import { commerce} from '../../../lib/commerce'
 import useStyles from "./styles";
-
 import AddressForm from "../AddressForm";
 import PaymentForm from "../PaymentForm";
 
 const steps = ["Shipping address", "Payment details"];
 
-const Checkout = () => {
+const Checkout = ({cart}) => {
   const [activeStep, setActiveStep] = useState(0)
+  const [checkoutToken, setcheckoutToken] = useState(null)
+
   const classes = useStyles();
+
+  useEffect(() => {
+    const generateToken = async () => {
+        try {
+            const token = await commerce.checkout.generateToken(cart.id, {type: 'cart'}) 
+
+            console.log(token)
+            setcheckoutToken(token)
+        } catch (error) {
+            
+        }
+    }
+
+    generateToken()
+  }, [cart])
 
   
  const Confirmation = () => (
@@ -29,7 +36,7 @@ const Checkout = () => {
  )
 
   const Form = () => activeStep === 0
-  ? <AddressForm />
+  ? <AddressForm checkoutToken={checkoutToken}/>
   : <PaymentForm />
 
   return (
@@ -47,7 +54,7 @@ const Checkout = () => {
               </Step>
             ))}
           </Stepper>
-          {activeStep === steps.length ? <Confirmation /> : <Form />}
+          {activeStep === steps.length ? <Confirmation /> : checkoutToken && <Form />}
         </Paper>
       </main>
     </>
