17eabc8f766bac1e6d8d19756916cb32a27f6d2e
glenvasa <70590043+glenvasa@users.noreply.github.com>
2021-09-14 11:09:57 -0400

    added handleCaptureCheckout and passed as prop to Checkout for child PaymentForm

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 const App = () => {
   const [products, setProducts] = useState([]);
   const [cart, setCart] = useState({});
+  const [order, setOrder] = useState({})
+  const [errorMessage, setErrorMessage] = useState('')
 
   const fetchProducts = async () => {
     const { data } = await commerce.products.list();
@@ -39,12 +41,25 @@ const App = () => {
   }
 
   const handleEmptyCart = async () => {
-  
       const { cart } = await commerce.cart.empty()
-
       setCart(cart)
   }
 
+ const refreshCart = async () => {
+   const newCart = await commerce.cart.refresh()
+   setCart(newCart)
+ }
+
+  const handleCaptureCheckout = async (checkoutTokenId, newOrder) => {
+    try {
+      const incomingOrder = await commerce.checkout.capture(checkoutTokenId, newOrder)
+
+      setOrder(incomingOrder)
+      refreshCart()
+    } catch (error) {
+      setErrorMessage(error.data.error.message)
+    }
+  }
 
 
   useEffect(() => {
@@ -69,7 +84,7 @@ const App = () => {
             />
           </Route>
           <Route exact path='/checkout'>
-              <Checkout cart={cart} />
+              <Checkout cart={cart} order={order} onCaptureCheckout={handleCaptureCheckout} error={errorMessage} />
           </Route>
         </Switch>
       </div>
